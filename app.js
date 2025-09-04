0c9186d9a02ec38945ab0cea855276105cee28f6
acreturs <acornett@gmx.de>
2025-09-03 21:40:29 -0300

    new port

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-});
+// Auf allen Interfaces lauschen
+app.listen(port, '0.0.0.0', () => console.log(`Server läuft auf Port ${port}`));
